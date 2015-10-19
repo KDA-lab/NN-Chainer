04eaae8b13774cb8eca1cc475577b14deb667885
柳本豪一 <hidekazu@manitou.local>
2015-10-19 22:08:30 +0900

    Reconstruct multilayer neural network to understand it easily.

--- multilayerNN.py
+++ multilayerNN.py
@@ -4,38 +4,38 @@ import numpy as np
 from chainer import Function, FunctionSet, gradient_check, Variable, optimizers, utils
 import chainer.functions as F
 
-nsize = 2
+x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
+y = np.array([[0], [1], [1], [0]], dtype=np.float32)
+datasize = len(x)
+hsize = 2
+
 model = FunctionSet(
-    x_to_h = F.Linear(2, nsize),
-    h_to_y = F.Linear(nsize, 1),
+    x_to_h = F.Linear(2, hsize),
+    h_to_y = F.Linear(hsize, 1),
 )
+optimizer = optimizers.Adam()
+optimizer.setup(model)
 
 def forward(x_data, y_data):
     x = Variable(x_data)
     t = Variable(y_data)
     h = F.sigmoid(model.x_to_h(x))
     y = F.sigmoid(model.h_to_y(h))
-    return F.mean_squared_error(y, t)
+    return y, F.mean_squared_error(y, t)
 
-optimizer = optimizers.Adam()
-optimizer.setup(model)
-
-datasize = 4
 batchsize = 2
-
-x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
-y = np.array([[0], [1], [1], [0]], dtype=np.float32)
-
 for epoch in range(20000):
-#    print 'Epoch %d' % epoch
     indexes = np.random.permutation(datasize)
-    sum = 0.0
+    sum = np.zeros(())
     for i in range(0, datasize, batchsize):
         x_batch = np.asarray(x[indexes[i:i+batchsize]])
         y_batch = np.asarray(y[indexes[i:i+batchsize]])
         optimizer.zero_grads()
-        loss = forward(x_batch, y_batch)
-        sum += loss.data
+        output, loss = forward(x_batch, y_batch)
+        sum += loss.data.reshape(())
         loss.backward()
         optimizer.update()
-    if ((epoch + 1) % 1000) == 0: print epoch+1, ":", sum/datasize
+    if ((epoch+1) % 1000) == 0: print 'Epoch %d: Error = %f' % (epoch+1, sum/datasize)
+
+output, loss = forward(x, y)
+print output.data
